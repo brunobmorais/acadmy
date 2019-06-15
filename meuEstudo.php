67c82f4edcd98c87e19f475ffcfd817167938feb
Caetano Burjack <caetano.burjack@gmail.com>
2019-06-15 14:41:01 -0300

    alter. fini

--- meuEstudo.php
+++ meuEstudo.php
@@ -11,14 +11,93 @@ include($raiz . "files/php/topo.php"); ?>
 <div class="container meu-container">
     <section class="section">
         <div class="section-body">
-            <!--<h2 class="section-title">This is Example Page</h2>
-            <p class="section-lead">This page is just an example for you to create your own page.</p>-->
-            <h3 class="tituloMovel"><img src="<?= $raiz ?>files/img/logo-preto.png" height="40px"/></h3>
-            <h2 class="section-title">Exercícios de Humanas</h2>
-            <p class="section-lead">História, Geografia, Filosofia e Sociologia.</p>
+
             <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="far fa-user"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Questões Resolvidas</h4>
+                            </div>
+                            <div class="card-body">
+                                286
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-primary">
+                            <i class="far fa-newspaper"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Acertos</h4>
+                            </div>
+                            <div class="card-body">
+                                202
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="far fa-file"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Erros</h4>
+                            </div>
+                            <div class="card-body">
+                                84
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-success">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Online Users</h4>
+                            </div>
+                            <div class="card-body">
+                                47
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
+
+            <div class="statistic-details mt-sm-4">
+                <div class="statistic-details-item">
+                    <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 7%</span>
+                    <div class="detail-value">$243</div>
+                    <div class="detail-name">Today's Sales</div>
+                </div>
+                <div class="statistic-details-item">
+                    <span class="text-muted"><span class="text-danger"><i class="fas fa-caret-down"></i></span> 23%</span>
+                    <div class="detail-value">$2,902</div>
+                    <div class="detail-name">This Week's Sales</div>
+                </div>
+                <div class="statistic-details-item">
+                    <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span>9%</span>
+                    <div class="detail-value">$12,821</div>
+                    <div class="detail-name">This Month's Sales</div>
+                </div>
+                <div class="statistic-details-item">
+                    <span class="text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 19%</span>
+                    <div class="detail-value">$92,142</div>
+                    <div class="detail-name">This Year's Sales</div>
+                </div>
             </div>
         </div>
     </section>
