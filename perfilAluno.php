45ce58a2a8b531d2f37378856c01f9d15cc29ee9
Caetano Burjack <caetano.burjack@gmail.com>
2019-06-15 02:21:09 -0300

    atualizando

--- perfilAluno.php
+++ perfilAluno.php
@@ -1,13 +1,17 @@
 <?php
-
-//string json contendo os dados de um funcionário
-$arquivo = "AlunosNotas.json";
+echo "testando:";
+//string json contendo os dados de um funcion?rio
+$arquivo = "files/json/AlunosNotas.json";
 
 $info = file_get_contents($arquivo);
-//print_r($info);exit;
+//print_r($info);
 //faz o parsing na string, gerando um objeto PHP
 $obj = json_decode($info);
-
-foreach($obj->discentes as $cadaAluno) {
-echo $cadaAluno->nome;
+$discentes = $obj->colegio->discentes;
+//print_r($discentes);
+foreach ($discentes as $cadaAluno) {
+    echo "<br>Aluno: " . $cadaAluno->nome;
+    foreach ($cadaAluno->notas as $nota) {
+        echo "<br>" . $nota->disciplina . " 1Âº Bim.: " . $nota->notas->b1;
+    };
 }
